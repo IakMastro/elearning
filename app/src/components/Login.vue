c033e1340a3fdebe02262dfafd68015ae4805696
Iakovos Mastrogiannopoulos <iakmastro@debian-BULLSEYE-live-builder-AMD64>
2022-04-23 20:46:52 +0300

    login component implemented

--- app/src/components/Login.vue
+++ app/src/components/Login.vue
@@ -1,39 +1,39 @@
 <template>
   <div class="login">
     <b-card>
-    <div>
-      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-        <b-form-group
-          id="input-group-1"
-          label="ID:"
-          label-for="input-1"
-        >
-          <b-form-input
-            id="input-1"
-            v-model="form.id"
-            type="text"
-            placeholder="Enter your ID"
-            required
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          id="input-group-2"
-          label="Password:"
-          label-for="input-2"
-        >
-          <b-form-input
-            id="input-2"
-            v-model="form.password"
-            type="password"
-            placeholder="Enter your password"
-            required
-          ></b-form-input>
-        </b-form-group>
-        <b-button type="submit" variant="primary">Log In</b-button>
-        <b-button type="reset" variant="danger">Reset Form</b-button>
-      </b-form>
-    </div>
-  </b-card>
+      <div>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+          <b-form-group
+            id="input-group-1"
+            label="ID:"
+            label-for="input-1"
+          >
+            <b-form-input
+              id="input-1"
+              v-model="form.id"
+              type="text"
+              placeholder="Enter your ID"
+              required
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group
+            id="input-group-2"
+            label="Password:"
+            label-for="input-2"
+          >
+            <b-form-input
+              id="input-2"
+              v-model="form.password"
+              type="password"
+              placeholder="Enter your password"
+              required
+            ></b-form-input>
+          </b-form-group>
+          <b-button type="submit" variant="primary">Log In</b-button>
+          <b-button type="reset" variant="danger">Reset Form</b-button>
+        </b-form>
+      </div>
+    </b-card>
   </div>
 </template>
 
